8aa586549625532e637fd6c6fd70de666f7d7d74
Justin Malčić <j.malcic@me.com>
2018-07-05 15:21:54 +0200

    Improve views for presentations

--- vendor/plugins/collect/app/controllers/spina/collect/presentations_controller.rb
+++ vendor/plugins/collect/app/controllers/spina/collect/presentations_controller.rb
@@ -1,12 +1,15 @@
 module Spina
   module Collect
     class PresentationsController < ::Spina::ApplicationController
+
       def index
         @presentations = Presentation.all
       end
+
       def show
-        @presentations ||= Presentation.find(params[:id])
+        @presentation ||= Presentation.find(params[:id])
       end
+
     end
   end
 end
--- vendor/plugins/collect/config/routes.rb
+++ vendor/plugins/collect/config/routes.rb
@@ -2,8 +2,8 @@ Spina::Engine.routes.draw do
   namespace :admin, path: Spina.config.backend_path do
     resources :presentations, except: [:show]
   end
+
   namespace :collect do
-    root to: 'presentations#index'
-    get ':id', to: 'presentations#show', as: :presentations
+    resources :presentations, only: [:index, :show]
   end
 end
