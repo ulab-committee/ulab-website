2b1e69d587d250d92e551254a8a07633b6902c65
Justin Malčić <j.malcic@me.com>
2018-07-21 07:49:12 +0200

    Remove redundant config

--- vendor/plugins/collect/config/initializers/assets.rb
+++ vendor/plugins/collect/config/initializers/assets.rb
@@ -14,8 +14,5 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
 
 # Compiler options
-Rails.application.config.opal.method_missing           = true
-Rails.application.config.opal.optimized_operators      = true
-Rails.application.config.opal.arity_check              = !Rails.env.production?
-Rails.application.config.opal.const_missing            = true
+Rails.application.config.opal.arity_check = !Rails.env.production?
 Rails.application.config.opal.dynamic_require_severity = :ignore
