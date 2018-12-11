e35051bdd84aacaa24a1eef0adb671df60be065d
Justin Malčić <j.malcic@me.com>
2018-12-11 18:11:58 +0000

    Add page translations

---
+++ db/migrate/20181211170042_translate_british_english_pages.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+class TranslateBritishEnglishPages < ActiveRecord::Migration[5.2] # :nodoc:
+  def up
+    Rake::Task[:'ulab_website:create_translations'].invoke
+  end
+
+  def down
+    Spina::Page.find_each do |page|
+      @translation = page.translations.find_by locale: :'en-GB'
+      @translation&.destroy
+    end
+  end
+end
---
+++ lib/tasks/ulab_website_tasks.rake
@@ -0,0 +1,12 @@
+namespace :ulab_website do
+  desc "Create translations for pages"
+  task create_translations: :environment do
+    Spina.locales.each do |locale|
+      I18n.locale = locale
+      Spina::Page.find_each do |page|
+        page.title ||= page.title locale: I18n.default_locale
+        page.save!
+      end
+    end
+  end
+end
