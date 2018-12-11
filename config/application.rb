0b3dd081fbf7574ef886ae80f11bf67af8115dc0
Justin Malčić <j.malcic@me.com>
2018-12-11 19:41:31 +0000

    Set default locale

--- config/application.rb
+++ config/application.rb
@@ -14,6 +14,8 @@ module UlabWebsite
     # Serve static assets
     config.serve_static_assets = true
 
+    config.i18n.default_locale = :'en-GB'
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
