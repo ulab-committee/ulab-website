1307d8bbf1a57d26f565c63e7451e0afab56ccf8
Justin Malčić <j.malcic@me.com>
2019-02-02 01:15:45 +0000

    Remove unnecessary config

--- config/puma.rb
+++ config/puma.rb
@@ -1,5 +1,3 @@
-require 'barnes'
-
 # Puma can serve each request in a thread from an internal thread pool.
 # The `threads` method setting takes two numbers: a minimum and maximum.
 # Any libraries that use thread pools should be configured to match
@@ -39,9 +37,4 @@ on_worker_boot do
   # Worker specific setup for Rails 4.1+
   # See: https://devcenter.heroku.com/articles/deploying-rails-applications-with-the-puma-web-server#on-worker-boot
   ActiveRecord::Base.establish_connection
-end
-
-before_fork do
-  # Worker specific setup
-  Barnes.start
 end
