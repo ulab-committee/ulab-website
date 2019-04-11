e52e9dc5250adcf31309f5231b80bac6d292c396
Justin Malčić <j.malcic@me.com>
2019-04-11 13:25:05 +0100

    Fix style

--- app/controllers/spina/conferences/presentations_controller.rb
+++ app/controllers/spina/conferences/presentations_controller.rb
@@ -5,22 +5,16 @@ module Spina
     # This class serves presentations as iCal
     class PresentationsController < ApplicationController
       def index
-        if params[:conference_id]
-          set_conference_presentations
-        else
-          set_presentations
-        end
+        params[:conference_id] ? set_conference_presentations : set_presentations
         respond_to(&:ics)
       end
 
       private
 
       def set_conference_presentations
-        @conference =
-          Spina::Conferences::Conference.find(params[:conference_id])
+        @conference = Spina::Conferences::Conference.find(params[:conference_id])
         @presentations = @conference.presentations
-        @calendar_name =
-          "#{current_account.name} #{@conference.name} Presentations"
+        @calendar_name = "#{current_account.name} #{@conference.name} Presentations"
       end
 
       def set_presentations
