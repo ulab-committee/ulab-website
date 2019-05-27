26cf536a19f7f72355b9c61a36c6a4b5648689df
Justin Malčić <j.malcic@me.com>
2019-05-27 02:08:08 +0100

    Improve style

--- app/javascript/packs/controllers/tabnav_controller.js
+++ app/javascript/packs/controllers/tabnav_controller.js
@@ -1,18 +1,18 @@
-import { Controller } from "stimulus"
+import { Controller } from 'stimulus'
 
 export default class extends Controller {
-  static targets = ["tab", "tabContent"]
+  static targets = ['tab', 'tabContent']
 
   toggleVisibility() {
     this.tabContentTargets.forEach(tabContent => {
-      if (`#${tabContent.id}` == location.hash) {
+      if (`#${tabContent.id}` === location.hash) {
         tabContent.removeAttribute('hidden')
       } else {
         tabContent.setAttribute('hidden', '')
       }
     })
     this.tabTargets.forEach(tab => {
-      if (tab.getAttribute('href') == location.hash) {
+      if (tab.getAttribute('href') === location.hash) {
         tab.classList.add('selected')
         tab.setAttribute('aria-current', 'page')
       } else {
