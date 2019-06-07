3ec9334d61e0542e5828d2d9b280b6164b8a3642
Justin Malčić <j.malcic@me.com>
2019-06-07 18:40:13 +0100

    Use Sidekiq

--- config/application.rb
+++ config/application.rb
@@ -15,6 +15,8 @@ module UlabWebsite
     config.i18n.default_locale = :'en-GB'
     config.active_storage.variant_processor = :vips
 
+    config.skylight.probes << 'active_job'
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers -- all .rb files in that directory are
     # automatically loaded after loading the framework and any gems in your application.
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -57,7 +57,7 @@ Rails.application.configure do
   config.cache_store = :redis_cache_store, { url: ENV['REDIS_URL'] }
 
   # Use a real queuing backend for Active Job (and separate queues per environment).
-  config.active_job.queue_adapter = :resque
+  config.active_job.queue_adapter = :sidekiq
   # config.active_job.queue_name_prefix = "ulab_website__production"
 
   config.action_mailer.perform_caching = false
