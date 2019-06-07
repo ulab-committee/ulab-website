b533712668fd1657477d4d15a2393d62008de073
Justin Malčić <j.malcic@me.com>
2019-06-07 19:04:03 +0100

    Remove require

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 # frozen_string_literal: true
 
-require 'resque/server'
-
 Rails.application.routes.draw do
   mount Spina::Engine => '/'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
