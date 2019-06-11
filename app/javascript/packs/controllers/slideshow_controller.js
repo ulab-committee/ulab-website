9aeb7564234ee117ca7c0ea14ef3e0a6e11b15cb
Justin Malčić <j.malcic@me.com>
2019-06-11 22:20:26 +0100

    Refactor JS

--- app/javascript/packs/controllers/slideshow_controller.js
+++ app/javascript/packs/controllers/slideshow_controller.js
@@ -3,32 +3,33 @@ import { Controller } from 'stimulus'
 export default class extends Controller {
   static targets = ['slide']
 
+  get incrementer() {
+    return Number.parseInt(this.data.get('incrementer'))
+  }
+
+  set incrementer(value) {
+    this.data.set('incrementer', value.toString())
+    this.showSlide()
+  }
+
   connect() {
-    this.showFirstSlide()
+    this.showSlide()
     if (this.data.has('advance')) {
       setInterval(() => this.next(), 10000)
     }
   }
 
-  showFirstSlide() {
-    this.showSlide()
-  }
-
   showSlide() {
-    this.slideTargets.forEach((target, index) => {
-      target.style.display = index === Number.parseInt(this.data.get('incrementer')) ? 'block' : 'none'
-    })
+    this.slideTargets.forEach((target, index) => target.style.display = index === this.incrementer ? 'block' : 'none')
   }
 
   next() {
-    const incrementer = Number.parseInt(this.data.get('incrementer'))
-    this.data.set('incrementer', incrementer === this.slideTargets.length - 1 ? '0' : `${incrementer + 1}`)
-    this.showSlide()
+    const incrementer = this.incrementer
+    this.incrementer = incrementer === this.slideTargets.length - 1 ? 0 : incrementer + 1
   }
 
   previous() {
-    const incrementer = Number.parseInt(this.data.get('incrementer'))
-    this.data.set('incrementer', incrementer === 0 ? `${this.slideTargets.length - 1}` : `${incrementer - 1}`)
-    this.showSlide()
+    const incrementer = this.incrementer
+    this.incrementer = incrementer === 0 ? this.slideTargets.length - 1 : incrementer - 1
   }
 }
