0385f628fa042246c819d4c1b64ada905b1a68c4
Justin Malčić <j.malcic@me.com>
2020-10-31 11:17:08 +0000

    Update seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,99 +6,42 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+# frozen_string_literal: true
+
 module Spina
-  Account.first_or_create name: 'ULAB', theme: 'conference'
+  Account.first_or_create name: 'ULAB', theme: 'conferences_primer_theme'
   User.first_or_create name: 'Joe', email: 'someone@someaddress.com', password: 'password', admin: true
-  module Conferences
-    def self.conference_parts
-      conference = Conference.new
-      model_parts(Conference).collect { |part| conference.part(part) }
+  module Admin
+    module Conferences
+      university_of_atlantis, university_of_shangri_la =
+        Institution.create! [{ name: 'University of Atlantis', city: 'Atlantis' },
+                             { name: 'University of Shangri-La', city: 'Shangri-La' }]
+      lecture_block2, _lecture_block3, _lecture_block_entrance =
+        Room.create! [{ building: 'Lecture block', number: '2', institution: university_of_atlantis },
+                      { building: 'Lecture block', number: '3', institution: university_of_atlantis },
+                      { building: 'Lecture block', number: 'entrance', institution: university_of_atlantis }]
+      _medical_school_g14, _medical_school_g152, _medical_school_g16 =
+        Room.create! [{ building: 'Medical school', number: 'G.14', institution: university_of_shangri_la },
+                      { building: 'Medical school', number: 'G.152', institution: university_of_shangri_la },
+                      { building: 'Medical school', number: 'G.16', institution: university_of_shangri_la }]
+      uoa_conference, uos_conference =
+        Conference.create! [{ start_date: '2017-04-07', finish_date: '2017-04-09', name: 'University of Atlantis 2017' },
+                            { start_date: '2018-04-09', finish_date: '2018-04-11', name: 'University of Shangri-La 2018' }]
+      _plenary1, _poster1, talk1, _plenary2, _poster2, _talk2 =
+        PresentationType.create! [{ name: 'Plenary', minutes: 80, conference: uoa_conference },
+                                  { name: 'Poster', minutes: 30, conference: uoa_conference },
+                                  { name: 'Talk', minutes: 20, conference: uoa_conference },
+                                  { name: 'Plenary', minutes: 80, conference: uos_conference },
+                                  { name: 'Poster', minutes: 30, conference: uos_conference },
+                                  { name: 'Talk', minutes: 20, conference: uos_conference }]
+      session = Session.create! presentation_type: talk1, room: lecture_block2, name: 'Session'
+      joe_bloggs = Delegate.create! first_name: 'Joe', last_name: 'Bloggs', email_address: 'someone@someaddress.com',
+                                    institution: university_of_atlantis,
+                                    dietary_requirements: [DietaryRequirement.new(name: 'Pescetarian')],
+                                    conferences: [uoa_conference, uos_conference]
+      Presentation.create! title: 'The Asymmetry and Antisymmetry of Syntax', date: '2017-04-07', start_time: '10:00',
+                           abstract: 'Lorem ipsum', presenters: [joe_bloggs],
+                           session: session
     end
-
-    def self.presentation_parts
-      presentation = Presentation.new
-      model_parts(Presentation).collect { |part| presentation.part(part) }
-    end
-
-    def self.model_parts(klass)
-      current_theme = ::Spina::THEMES.find { |theme| theme.name == ::Spina::Account.first.theme }
-      current_theme.page_parts.select { |part| part[:name].in? current_theme.models[klass.to_s.to_sym][:parts] }
-    end
-
-    Institution.create! name: 'University of Atlantis', city: 'Atlantis',
-                        rooms: [Room.new(building: 'Lecture block', number: '2'),
-                                Room.new(building: 'Lecture block', number: '3'),
-                                Room.new(building: 'Lecture block', number: 'entrance')]
-    Institution.create! name: 'University of Shangri-La', city: 'Shangri-La',
-                        rooms: [Room.new(building: 'Medical school', number: 'G.14'),
-                                Room.new(building: 'Medical school', number: 'G.152'),
-                                Room.new(building: 'Medical school', number: 'G.16')]
-    Conference.create! institution: Institution.find_by(name: 'University of Atlantis'), start_date: '2017-04-07',
-                       finish_date: '2017-04-09', parts: conference_parts,
-                       rooms: Room.includes(:institution).where(
-                         spina_conferences_institutions: { name: 'University of Atlantis' }
-                       )
-    Conference.create! institution: Institution.find_by(name: 'University of Shangri-La'), start_date: '2018-04-09',
-                       finish_date: '2018-04-11', parts: conference_parts,
-                       rooms: Room.includes(:institution).where(
-                         spina_conferences_institutions: { name: 'University of Shangri-La' }
-                       )
-    PresentationType.create! [{
-      name: 'Plenary', minutes: 80, conference: Conference.includes(:institution).find_by(
-        spina_conferences_institutions: { name: 'University of Atlantis' }
-      ), room_possessions: RoomPossession.includes(:room, :institution).where(
-        spina_conferences_institutions: { name: 'University of Atlantis' },
-        spina_conferences_rooms: { building: 'Lecture block', number: '3' }
-      )
-    }, {
-      name: 'Poster', minutes: 30, conference: Conference.includes(:institution).find_by(
-        spina_conferences_institutions: { name: 'University of Atlantis' }
-      ), room_possessions: RoomPossession.includes(:room, :institution).where(
-        spina_conferences_institutions: { name: 'University of Atlantis' },
-        spina_conferences_rooms: { building: 'Lecture block', number: 'entrance' }
-      )
-    }, {
-      name: 'Talk', minutes: 20, conference: Conference.includes(:institution).find_by(
-        spina_conferences_institutions: { name: 'University of Atlantis' }
-      ), room_possessions: RoomPossession.includes(:room, :institution).where(
-        spina_conferences_institutions: { name: 'University of Atlantis' },
-        spina_conferences_rooms: { building: 'Lecture block', number: %w[2 3] }
-      )
-    }, {
-      name: 'Plenary', minutes: 80, conference: Conference.includes(:institution).find_by(
-        spina_conferences_institutions: { name: 'University of Shangri-La' }
-      ), room_possessions: RoomPossession.includes(:room, :institution).where(
-        spina_conferences_institutions: { name: 'University of Shangri-La' },
-        spina_conferences_rooms: { building: 'Medical school', number: 'G.152' }
-      )
-    }, {
-      name: 'Poster', minutes: 30, conference: Conference.includes(:institution).find_by(
-        spina_conferences_institutions: { name: 'University of Shangri-La' }
-      ), room_possessions: RoomPossession.includes(:room, :institution).where(
-        spina_conferences_institutions: { name: 'University of Shangri-La' },
-        spina_conferences_rooms: { building: 'Medical school', number: 'G.14' }
-      )
-    }, {
-      name: 'Talk', minutes: 20, conference: Conference.includes(:institution).find_by(
-        spina_conferences_institutions: { name: 'University of Shangri-La' }
-      ), room_possessions: RoomPossession.includes(:room, :institution).where(
-        spina_conferences_institutions: { name: 'University of Shangri-La' },
-        spina_conferences_rooms: { building: 'Medical school', number: %w[G.152 G.16] }
-      )
-    }]
-    Delegate.create! first_name: 'Joe', last_name: 'Bloggs', email_address: 'someone@someaddress.com',
-                     institution: Institution.find_by(name: 'University of Atlantis'),
-                     dietary_requirements: [DietaryRequirement.new(name: 'Pescetarian')],
-                     conferences: Conference.includes(:institution).where(
-                       spina_conferences_institutions: { name: ['University of Atlantis', 'University of Shangri-La'] }
-                     )
-    Presentation.create! title: 'The Asymmetry and Antisymmetry of Syntax', date: '2017-04-07', start_time: '10:00',
-                         abstract: 'Lorem ipsum', presenters: Delegate.where(first_name: 'Joe', last_name: 'Bloggs'),
-                         parts: presentation_parts,
-                         room_use: RoomUse.includes(:room, :presentation_type, :institution).find_by(
-                           spina_conferences_rooms: { building: 'Lecture block', number: '2' },
-                           spina_conferences_presentation_types: { name: 'Talk' },
-                           spina_conferences_institutions: { name: 'University of Atlantis' }
-                         )
   end
 end
