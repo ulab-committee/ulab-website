7f44563f4184441291c530bffd669835be556dbb
Justin Malčić <j.malcic@me.com>
2021-04-14 21:24:35 +0100

    Migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_14_172542) do
+ActiveRecord::Schema.define(version: 2021_04_13_212755) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -54,6 +54,7 @@ ActiveRecord::Schema.define(version: 2021_03_14_172542) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.boolean "robots_allowed", default: false
+    t.jsonb "json_attributes"
   end
 
   create_table "spina_attachment_collections", id: :serial, force: :cascade do |t|
@@ -86,6 +87,7 @@ ActiveRecord::Schema.define(version: 2021_03_14_172542) do
     t.daterange "dates", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.jsonb "json_attributes"
   end
 
   create_table "spina_conferences_conferences_delegates", id: false, force: :cascade do |t|
@@ -251,6 +253,7 @@ ActiveRecord::Schema.define(version: 2021_03_14_172542) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.datetime "start_datetime", null: false
+    t.jsonb "json_attributes"
     t.index ["session_id"], name: "index_spina_conferences_presentations_on_session_id"
   end
 
@@ -421,6 +424,7 @@ ActiveRecord::Schema.define(version: 2021_03_14_172542) do
     t.integer "position"
     t.boolean "active", default: true
     t.integer "resource_id"
+    t.jsonb "json_attributes"
     t.index ["resource_id"], name: "index_spina_pages_on_resource_id"
   end
 
@@ -432,6 +436,7 @@ ActiveRecord::Schema.define(version: 2021_03_14_172542) do
     t.string "order_by"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.jsonb "slug"
     t.index ["parent_page_id"], name: "index_spina_resources_on_parent_page_id"
   end
 
