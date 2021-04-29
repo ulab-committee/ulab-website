6424fceeaa95bda4ccc93867b5935d5c44f21cc9
Justin Malčić <j.malcic@me.com>
2021-04-29 20:44:19 +0100

    Proxy files

--- config/application.rb
+++ config/application.rb
@@ -16,6 +16,8 @@ module UlabWebsite
     
     config.skylight.environments << 'staging'
     config.skylight.probes += %w[redis active_job]
+    
+    config.active_storage.resolve_model_to_route = :rails_storage_proxy
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers -- all .rb files in that directory are
