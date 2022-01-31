f1996b23b771d4e222825b3ce406ddac21683e64
Louis Van Steene <louis@lvs.me.uk>
2022-01-31 18:56:22 +0000

    Reorder routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,14 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  mount Spina::Engine => '/'
-
   # manual redirecting of old routes
   get '/issues/1', to: redirect('/volumes/1/issues/1')
   get '/issues/1/articles/1', to: redirect('/volumes/1/issues/1/articles/1')
   get '/issues/1/articles/2', to: redirect('/volumes/1/issues/1/articles/2')
   get '/issues/1/articles/3', to: redirect('/volumes/1/issues/1/articles/3')
+  get '/issues/1/articles/1.pdf', to: redirect('/volumes/1/issues/1/articles/1.pdf')
+  get '/issues/1/articles/2.pdf', to: redirect('/volumes/1/issues/1/articles/2.pdf')
+  get '/issues/1/articles/3.pdf', to: redirect('/volumes/1/issues/1/articles/3.pdf')
+
+  mount Spina::Engine => '/'
 end
