c984ca8d07b2f5781afbe7e20c9aaa4c962223e5
Louis Van Steene <louis@lvs.me.uk>
2022-01-31 19:00:26 +0000

    Correctly namespace manual routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,13 +2,13 @@
 
 Rails.application.routes.draw do
   # manual redirecting of old routes
-  get '/issues/1', to: redirect('/volumes/1/issues/1')
-  get '/issues/1/articles/1', to: redirect('/volumes/1/issues/1/articles/1')
-  get '/issues/1/articles/2', to: redirect('/volumes/1/issues/1/articles/2')
-  get '/issues/1/articles/3', to: redirect('/volumes/1/issues/1/articles/3')
-  get '/issues/1/articles/1.pdf', to: redirect('/volumes/1/issues/1/articles/1.pdf')
-  get '/issues/1/articles/2.pdf', to: redirect('/volumes/1/issues/1/articles/2.pdf')
-  get '/issues/1/articles/3.pdf', to: redirect('/volumes/1/issues/1/articles/3.pdf')
+  get '/journal/issues/1', to: redirect('/journal/volumes/1/issues/1')
+  get '/journal/issues/1/articles/1', to: redirect('/journal/volumes/1/issues/1/articles/1')
+  get '/journal/issues/1/articles/2', to: redirect('/journal/volumes/1/issues/1/articles/2')
+  get '/journal/issues/1/articles/3', to: redirect('/journal/volumes/1/issues/1/articles/3')
+  get '/journal/issues/1/articles/1.pdf', to: redirect('/journal/volumes/1/issues/1/articles/1.pdf')
+  get '/journal/issues/1/articles/2.pdf', to: redirect('/journal/volumes/1/issues/1/articles/2.pdf')
+  get '/journal/issues/1/articles/3.pdf', to: redirect('/journal/volumes/1/issues/1/articles/3.pdf')
 
   mount Spina::Engine => '/'
 end
